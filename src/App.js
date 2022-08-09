578bd6891255350ebb4fc18146dc65dba12a3e3c
Taekyung An <atk0426@naver.com>
2022-08-10 01:14:57 +0900

    saving

--- src/App.js
+++ src/App.js
@@ -2,21 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const proverbs=['좌절감으로 배움을 늦추지마라', 'Stay Hungry, Stay Foolish', '누군가를 위해 당신의 삶을 허비하지 마세요', '빼앗긴 들에도 봄은 온다', '계획이란 미래에 대한 현재의 결정이다', '아마추어는 문제를 복잡하게 만들고, 프로는 문제를 단순화 시킨다', 'Menento mori']
+
+  const getRandomIndex = (length) => {
+    return parseInt(Math.random()*length)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>src/App.js</code> 파일을 고치고 저장하면 새로운 명언을 볼 수 있습니다.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          {proverbs[getRandomIndex(proverbs.length)]}
       </header>
     </div>
   );
